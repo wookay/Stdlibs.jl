8647e9729098faab54e6d01671907fceac6a5f7c
WooKyoung Noh <wookay.noh@gmail.com>
2018-09-29 14:38:54 +0900

    init

---
+++ src/Stdlibs.jl
@@ -0,0 +1,32 @@
+module Stdlibs
+
+using Base64
+using CRC32c
+using Dates
+using DelimitedFiles
+using Distributed
+using FileWatching
+using InteractiveUtils
+using LibGit2
+using Libdl
+using LinearAlgebra
+using Logging
+using Markdown
+using Mmap
+using Pkg
+using Printf
+using Profile
+using REPL
+using Random
+using SHA
+using Serialization
+using SharedArrays
+using Sockets
+using SparseArrays
+using Statistics
+using SuiteSparse
+using Test
+using UUIDs
+using Unicode
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,4 @@
+using Stdlibs
+using .Stdlibs.Test # @test
+
+@test 1+2 == 3
