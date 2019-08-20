9ba855c9daedd29105a579b64a84de1c68c21cdc
WooKyoung Noh <wookay.noh@gmail.com>
2019-08-21 08:14:23 +0900

    travis

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,20 @@
-using Stdlibs
-using .Stdlibs.Test # @test
+module test_stdlibs
 
-@test 1+2 == 3
+using Stdlibs
+using .Stdlibs.Test # @testset @test
+
+@testset "test parentmodule" begin
+    @test parentmodule(Stdlibs.Test) !== Stdlibs
+end
+
+pkg = Base.PkgId(Base.UUID("44cfe95a-1eb2-52ea-b672-e2afdf69b78f"), "Pkg")
+loaded = Base.loaded_modules[pkg]
+using .loaded
+
+if VERSION >= v"1.4.0-DEV"
+    @testset "test artifacts" begin
+        @test loaded.Artifacts isa Module
+    end
+end
+
+end # module test_stdlibs
